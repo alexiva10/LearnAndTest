dbfddee3230eead05a3f7fa7952eb40b17153035
alexiva10 <alexbogdanivanescu@gmail.com>
2022-08-18 10:51:16 +0300

    Initial Commit

---
+++ LearnAndTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LearnAndTest
+//
+//  Created by Alex Ivanescu on 18.08.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LearnAndTest/LearnAndTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnAndTestApp.swift
+//  LearnAndTest
+//
+//  Created by Alex Ivanescu on 18.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct LearnAndTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
