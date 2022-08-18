1d8c09c7b2b00c320a8004921c7e350ff5ce9669
alexiva10 <alexbogdanivanescu@gmail.com>
2022-08-18 11:07:46 +0300

    Setup project

---
+++ LearnAndTest/ViewModels/ContentModel.swift
@@ -0,0 +1,12 @@
+//
+//  ContentModel.swift
+//  LearnAndTest
+//
+//  Created by Alex Ivanescu on 18.08.2022.
+//
+
+import Foundation
+
+class ContentModel: ObservableObject {
+    
+}
--- LearnAndTest/Views/HomeView.swift
+++ LearnAndTest/Views/HomeView.swift
@@ -7,15 +7,18 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct HomeView: View {
+    
+    @EnvironmentObject var model:ContentModel
+    
     var body: some View {
         Text("Hello, world!")
             .padding()
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        HomeView()
     }
 }
