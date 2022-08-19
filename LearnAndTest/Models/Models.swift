ef707a64e9096479b9aceeb2d1048a3e6d9fab25
alexiva10 <alexbogdanivanescu@gmail.com>
2022-08-19 10:09:22 +0300

    Add assets Add Models Parse JSON data

---
+++ LearnAndTest/Models/Models.swift
@@ -0,0 +1,48 @@
+//
+//  Models.swift
+//  LearnAndTest
+//
+//  Created by Alex Ivanescu on 19.08.2022.
+//
+
+import Foundation
+
+class Module : Decodable, Identifiable {
+    var id: Int
+    var category: String
+    var content: Content
+    var test: Test
+}
+
+class Content : Decodable, Identifiable {
+    var id: Int
+    var image: String
+    var time: String
+    var description: String
+    var lesson: [Lesson]
+}
+
+class Lesson : Decodable, Identifiable {
+    var id: Int
+    var title: String
+    var video: String
+    var duration: String
+    var explination: String
+}
+
+class Test : Decodable, Identifiable {
+    var id: Int
+    var image: String
+    var time: String
+    var description: String
+    var question: [Question]
+}
+
+class Question : Decodable, Identifiable {
+    var id: Int
+    var content: String
+    var correctIndex: Int
+    var answers: [String]
+}
+
+
--- LearnAndTest/ViewModels/ContentModel.swift
+++ LearnAndTest/ViewModels/ContentModel.swift
@@ -9,4 +9,31 @@ import Foundation
 
 class ContentModel: ObservableObject {
     
+    @Published var modules = [Module]()
+    
+    var styleData: Data?
+    
+    init() {
+        getLocalData()
+    }
+    
+    func getLocalData() {
+        let jsonUrl = Bundle.main.url(forResource: "data", withExtension: "json")
+        do {
+            let jsonData = try Data(contentsOf: jsonUrl!)
+            let jsonDecoder = JSONDecoder()
+            let modules = try jsonDecoder.decode([Module].self, from: jsonData)
+            self.modules = modules
+        } catch {
+            print("Could't parse!")
+        }
+        
+        let styleUrl = Bundle.main.url(forResource: "style", withExtension: "html")
+        do {
+            let styleData = try Data(contentsOf: styleUrl!)
+            self.styleData = styleData
+        } catch {
+            print("Could't parse!")
+        }
+    }
 }
